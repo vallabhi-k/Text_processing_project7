96fe40d82b1843001cc178e33d145734b9cca818
Vallabhi Kamalia <vallabhikamalia@Vallabhis-MacBook-Air.local>
2023-06-10 23:26:40 -0700

    Rafactore code or use your message

--- project7/Minhash.java
+++ project7/Minhash.java
@@ -10,78 +10,69 @@ import java.util.*;
 
 public class Minhash {
 
-  public double jaccard(String fA, String fB) {
-    ArrayList<String> file1_words  = readFile(fA);
-    ArrayList<String> file2_words = readFile(fB);
-    HashSet<Integer> file1_shingles = new HashSet<>();
-    HashSet<Integer> file2_shingles = new HashSet<>();
+  public double calculateJaccardSimilarity(String fileA, String fileB) {
+    ArrayList<String> file1Words  = readFile(fileA);
+    ArrayList<String> file2Words = readFile(fileB);
+    HashSet<Integer> file1Shingles = new HashSet<>();
+    HashSet<Integer> file2Shingles = new HashSet<>();
     
-    for (int i = 0; i < file1_words.size(); i++)
-    {
-      String shingle = file1_words.get(i);
-      file1_shingles.add(shingle.hashCode());
+    for (int i = 0; i < file1Words.size(); i++) {
+      String shingle = file1Words.get(i);
+      file1Shingles.add(shingle.hashCode());
     }
 
-    for (int i = 0; i < file2_words.size(); i++)
-    {
-      String shingle = file2_words.get(i);
-      file2_shingles.add(shingle.hashCode());
+    for (int i = 0; i < file2Words.size(); i++) {
+      String shingle = file2Words.get(i);
+      file2Shingles.add(shingle.hashCode());
     }
 
-    int total_shingles = file1_shingles.size() + file2_shingles.size();
+    int totalShingles = file1Shingles.size() + file2Shingles.size();
 
-    int maxShingleID = (int)Math.pow(2,32)-1;
+    int maxShingleID = (int) Math.pow(2, 32) - 1;
     long nextPrime = 4294967311L;
 
-    int num_hashes = 99;
+    int numHashes = 99;
 
-    List<Integer> a = pickRandomCoeff(num_hashes, maxShingleID);
-    List<Integer> b = pickRandomCoeff(num_hashes, maxShingleID);
+    List<Integer> a = pickRandomCoeff(numHashes, maxShingleID);
+    List<Integer> b = pickRandomCoeff(numHashes, maxShingleID);
         
-    List<Long> sig_a = new ArrayList<>();
-    List<Long> sig_b = new ArrayList<>();
+    List<Long> sigA = new ArrayList<>();
+    List<Long> sigB = new ArrayList<>();
 
-    List<Integer> file1_shingles_arr = new ArrayList<Integer>(file1_shingles);
-    List<Integer> file2_shingles_arr = new ArrayList<Integer>(file2_shingles);
+    List<Integer> file1ShinglesArr = new ArrayList<>(file1Shingles);
+    List<Integer> file2ShinglesArr = new ArrayList<>(file2Shingles);
     
-        
-    for (int i = 0; i < num_hashes; i++) {
+    for (int i = 0; i < numHashes; i++) {
       long minHash = nextPrime + 1;
-      for (int j = 0; j < file1_shingles_arr.size(); j++) {
-          long hash = (long)((long) (a.get(i) * file1_shingles_arr.get(j) + b.get(i)) % nextPrime);
-          if (hash < minHash) {
-              minHash = hash;
-          }
-      }
-      sig_a.add(minHash);
-    }
-
-  for (int i = 0; i < num_hashes; i++) {
-    long minHash = nextPrime + 1;
-    for (int j = 0; j < file2_shingles_arr.size(); j++) {
-        long hash = (long)((long) (a.get(i) * file2_shingles_arr.get(j) + b.get(i)) % nextPrime);
+      for (int j = 0; j < file1ShinglesArr.size(); j++) {
+        long hash = (a.get(i) * file1ShinglesArr.get(j) + b.get(i)) % nextPrime;
         if (hash < minHash) {
-            minHash = hash;
+          minHash = hash;
         }
+      }
+      sigA.add(minHash);
     }
-    sig_b.add(minHash);
-  }
 
-
-  int count = 0;
-  for (int i = 0; i < num_hashes; i++)
-  {
-    if (sig_a.get(i).equals(sig_b.get(i)))
-    {
-      count++;
+    for (int i = 0; i < numHashes; i++) {
+      long minHash = nextPrime + 1;
+      for (int j = 0; j < file2ShinglesArr.size(); j++) {
+        long hash = (a.get(i) * file2ShinglesArr.get(j) + b.get(i)) % nextPrime;
+        if (hash < minHash) {
+          minHash = hash;
+        }
+      }
+      sigB.add(minHash);
     }
-  }
 
-  float sim = (float)(count) / (float)(num_hashes);
-  return sim;
+    int count = 0;
+    for (int i = 0; i < numHashes; i++) {
+      if (sigA.get(i).equals(sigB.get(i))) {
+        count++;
+      }
+    }
 
-
-  //  return 0.0;
+    float similarity = (float) count / (float) numHashes;
+    return similarity;
   }
 
   public static List<Integer> pickRandomCoeff(int k, int maxShingleID) {
@@ -89,27 +80,27 @@ public class Minhash {
     Random rand = new Random();
     
     while (randList.size() < k) {
-        int randIndex = rand.nextInt(maxShingleID);
-        randList.add(randIndex);
+      int randIndex = rand.nextInt(maxShingleID);
+      randList.add(randIndex);
     }
     
     return new ArrayList<>(randList);
   }
 
-  public static ArrayList<String> readFile(String fileName){
-    ArrayList<String> file_words = new ArrayList<>();
+  public static ArrayList<String> readFile(String fileName) {
+    ArrayList<String> fileWords = new ArrayList<>();
     try {
-      File myObj = new File(fileName);
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        file_words.add(myReader.nextLine());        
+      File file = new File(fileName);
+     
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        fileWords.add(scanner.nextLine());        
       }
-      myReader.close();
+      scanner.close();
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-    return file_words;
+    return fileWords;
   }
-
 }
